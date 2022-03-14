9e1fed04e2d39653a7004afa768f6fcfc752f369
Seyed Abdolreza S Mohseni (DJPR) <Seyed.Abdolreza.Mohseni@ecodev.vic.gov.au>
2022-03-14 21:09:23 +1100

    for loop

---
+++ for2.py
@@ -0,0 +1,13 @@
+
+
+Age=[34,65,67,23,14,16];
+Height=[187,176,167,178,156,168]
+
+for i in range(1,len(Age)):
+    
+    for j in range(1,len(Height)):
+        
+        print("Age+Height=",Age[i]+Height[j])
+        
+        
+        
