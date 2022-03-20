67cc3a24228b351705fc4eb982408905056db43c
Seyed Abdolreza S Mohseni (DJPR) <Seyed.Abdolreza.Mohseni@ecodev.vic.gov.au>
2022-03-20 18:40:44 +1100

    Delta

---
+++ delta.py
@@ -0,0 +1,20 @@
+
+
+def delta(a,b,c):
+    
+    delta=b**2-4*a*c
+    
+    if delta>0:
+        
+        print("Two possible answers are available")
+        
+    elif delta==0:
+            
+            print("Ony one answer is available")
+            
+    else:
+                
+                print("No answer in real numbers is available")
+    
+    
+    
