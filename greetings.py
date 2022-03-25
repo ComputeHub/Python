0345340bd6d291c6b9ae3544c888f958518561e3
Seyed Abdolreza S Mohseni (DJPR) <Seyed.Abdolreza.Mohseni@ecodev.vic.gov.au>
2022-03-25 15:53:19 +1100

    greetings git commit -m greetings

---
+++ greetings.py
@@ -0,0 +1,19 @@
+
+
+def greetings():
+    '''
+    Enter the name as in the argument of the function
+
+    Parameters
+    ----------
+    a : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
+    a=input("Enter the name:")
+    a=str(a)
+    print("Hello "+a)
